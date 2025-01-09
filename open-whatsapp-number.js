ea80a08c44bd2f06a8df981e066bb6dd0f94c9b2
nicolassanmar <nicolassanmar@gmail.com>
2025-01-09 11:44:18 -0300

    renames

--- open-whatsapp-number.js
+++ open-whatsapp-number.js
@@ -1,16 +1,13 @@
 (() => {
   // Get the phone number alert
-  const phoneNumber = prompt("Enter the phone number");
-  const phoneNumberWithoutSpaces = phoneNumber.replace(/\s/g, "");
-  const phoneNumberWithoutCountryCode = phoneNumberWithoutSpaces.replace(
-    "+598",
-    "0"
-  );
-  const phoneNumberWithCountryCode = `598${phoneNumberWithoutCountryCode}`;
+  const phoneNumInput = prompt("Enter the phone number");
+  const phoneNumWithoutSpaces = phoneNumInput.replace(/\s/g, "");
+  const phoneNumWithoutCountryCode = phoneNumWithoutSpaces.replace("+598", "0");
+  const phoneNumWithCountryCode = `598${phoneNumWithoutCountryCode}`;
 
   // Open the WhatsApp chat in the same tab
   window.open(
-    `https://web.whatsapp.com/send/?phone=${phoneNumberWithCountryCode}`,
+    `https://web.whatsapp.com/send/?phone=${phoneNumWithCountryCode}`,
     "_self"
   );
 })();
